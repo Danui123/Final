f6c33e9657520db6da266331c69efebc3d056803
Danui123 <150238722+Danui123@users.noreply.github.com>
2025-04-22 14:58:36 +0530

    Update user.js

--- user.js
+++ user.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const user = { username, email, password };
         localStorage.setItem("user", JSON.stringify(user));
         alert("Registration Successful! Please Login.");
-        window.location.href = "index.html";
+        window.location.href = "login.html";
       });
     }
   
